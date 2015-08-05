6c2694a16d4bf43a5bd2af3920075b6c351e574c
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2015-08-05 18:20:25 +0900

    add dirty js code for eslint

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,13 @@
+// Loads all Bootstrap javascripts
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require footer_fixed.js
+//= require browser_timezone_rails/application.js
+
+$("a.github_auth").click(function () {
+  ga('send', 'event', 'button', 'click', {'page': '/users/auth/github'})
+  ga('send', 'event', 'button', 'click', {'page': '/users/auth/github'})
+});
+
+mixpanel.track_links("a.github_auth", "Click GitHub Auth");
